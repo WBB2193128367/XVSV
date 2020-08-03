364f13073cce9974a36379337238ed02c9728506
WBB2193128367 <wang09181821@icloud.com>
2020-08-03 11:52:03 +0800

    XVSX(v.1.0)

---
+++ GUI/gui.py
@@ -0,0 +1,82 @@
+from tkinter import ttk
+from tkinter import *
+import  os
+import tkinter.filedialog
+from bao import method
+from bao import a_ico as a
+from  bao import  e_jpg as e
+from bao import  c_jpg as c
+#对图片的大小进行设置
+root = Tk() #创建主窗体
+root.title("xvideos爬虫!")
+root.geometry("350x540+750+200")
+#root.attributes("-alpha",0.6)
+k=os.path.abspath(os.path.join(os.getcwd(),'..'))
+method.decodepicture(k+'/image/a',a.img,'.ico')
+root.iconbitmap(k+'/image/a.ico')
+root.resizable(0,0)
+
+huabu=tkinter.Canvas(root,width=350,height=540)
+method.decodepicture(k+'/image/e',e.img,'.jpg')
+tupian=method.get_image(k+'/image/e.jpg',350,540)
+huabu.create_image(175,270,image=tupian)
+huabu.pack()
+
+
+lb = Label(root, text="请选择系统或自定义类型:  ")
+lb.place(x=25,y=150)
+combobox=ttk.Combobox(root,width=17,state='readonly')
+combobox['values']=("系统类型","自定义类型")
+combobox.current(1)
+combobox.place(x=180,y=150)
+lb = Label(root, text="请输入要爬取的视频类型:  ")
+lb.place(x=25,y=185)
+entry = Entry(root)
+entry.place(x=180,y=185)
+lb = Label(root, text="请输入视频命名开始的序号:")
+lb.place(x=25,y=220)
+entry1 = Entry(root)
+entry1.place(x=180,y=220)
+lb = Label(root, text="请输入开始爬取的网页号:  ")
+lb.place(x=25,y=255)
+entry2 = Entry(root)
+entry2.place(x=180,y=255)
+
+lb = Label(root, text="请输入结束爬取的网页号:  ")
+lb.place(x=25,y=290)
+entry3 = Entry(root)
+entry3.place(x=180,y=290)
+lb = Label(root, text="请选择视频的存储位置:    ")
+lb.place(x=25,y=325)
+str4=StringVar()
+lb = Entry(root, textvariable=str4,width=14)
+lb.place(x=180,y=325)
+
+la=Label(root,text="爬取视频的画质:")
+la.place(x=70,y=360)
+combobox1=ttk.Combobox(root,width=10,state='readonly')
+combobox1['values']=("高","低")
+combobox1.current(1)
+combobox1.place(x=180,y=360)
+
+btn1 = Button(root, text="浏览..", width=4,height=1,cursor='pirate',command= lambda :method.liulan(str4))
+btn1.place(x=285,y=323)
+method.decodepicture(k+'/image/c',c.img,'.jpg')
+button_image=method.get_image(k+'/image/c.jpg',55,28)
+btn = Button(root, text="开始爬取",image=button_image,bd=8,cursor='pirate',relief="raised", fg='black', width=55,height=25,compound=tkinter.CENTER,command=lambda :method.get_photo(entry,entry1,entry2,entry3,str4,combobox,combobox1))
+btn.place(x=200,y=410)
+
+root.protocol("WM_DELETE_WINDOW",lambda :method.close_windows(root))
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ bao/method.py
@@ -0,0 +1,189 @@
+from tkinter import ttk
+from tkinter import *
+import tkinter.filedialog
+from tkinter import messagebox
+import requests
+import re
+import os
+import time
+import  base64
+from bao import  b_ico as b
+from bao import d_jpg as s
+from PIL import Image,ImageTk
+
+
+
+def decodepicture(a,b,c):
+    tmp = open(a+c, 'wb')  # 创建临时的文件
+    tmp.write(base64.b64decode(b))  ##把这个one图片解码出来，写入文件中去。
+    tmp.close()
+
+#对图片的大小进行设置
+def get_image(filename,width,height):
+    im=Image.open(filename).resize((width,height))
+    return ImageTk.PhotoImage(im)
+
+
+
+
+def get_code(combobox,entry,page):  # 获取网页源码
+
+    if combobox.get()=='自定义类型':
+
+        wbb = 'https://www.xvideos.com/?k='+entry.get()+'&p=' + str(page)
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36'}
+        response = requests.get(wbb, headers=headers)
+        response.encoding = 'utf-8'
+        html = response.text
+        return html
+    elif combobox.get()=='系统类型':
+        wbb = 'https://www.xvideos.com/c/' + entry.get() + "/" + str(page)
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36'}
+        response = requests.get(wbb, headers=headers)
+        response.encoding = 'utf-8'
+        html = response.text
+        return html
+    else:
+        tkinter.messagebox.showinfo('提示', '请选择系统类型或自定义类型!')
+
+
+def shaixuan(html):  # 对源码进行筛选
+    urls = re.findall('<div id=".*?" data-id="(.*?)" class="thumb-block ">', html, re.S)
+    return urls
+
+def liulan(str4):
+    m=tkinter.filedialog.askdirectory()
+    str4.set(m)
+    return m
+
+def close_windows(top):
+    k = os.path.abspath(os.path.join(os.getcwd(), '..'))
+    if tkinter.messagebox.askokcancel('退出','确认要退出吗？'):
+        a=['a','b']
+        b=['c','d','e']
+        for i in a:
+            os.remove(k+'/image/'+i+'.ico')
+        for i in b:
+            os.remove(k+'/image/'+i+'.jpg')
+        top.destroy()
+
+
+def get_photo(entry,entry1,entry2,entry3,str4,combobox,combobox1):
+    if entry.get()!='' and entry1.get()!='' and entry2.get()!='' and entry3.get()!='' and str4.get()!='' and combobox.get()!='':
+        top = Toplevel() #创建弹出式窗体
+        top.title('爬取页面')
+        top.geometry("350x540+360+200")
+        top.resizable(0,0)
+        k = os.path.abspath(os.path.join(os.getcwd(), '..'))
+        decodepicture(k+'/image/b',b.img,'.ico')
+        top.iconbitmap(k+'/image/b.ico')
+        huabu = tkinter.Canvas(top, width=350, height=540)
+        decodepicture(k+'/image/d',s.img,'.jpg')
+        tupian = get_image(k+'/image/d.jpg', 350, 540)
+        huabu.create_image(175, 270, image=tupian)
+        huabu.pack()
+        str1=StringVar()
+        str2=StringVar()
+        str3 = StringVar()
+        lb = Label(top, text='视频爬取进度:')
+        lb.place(x=10,y=180)
+        p1 = ttk.Progressbar(top, length=200, cursor='spider',
+                             mode="determinate",
+                             orient=HORIZONTAL,
+                             maximum=100
+                             )
+        p1.place(x=92,y=180)
+        lb = Label(top, textvariable=str1)
+        lb.place(x=10,y=100)
+        lb = Label(top, textvariable=str2)
+        lb.place(x=10,y=140)
+
+        lb = Label(top, textvariable=str3)
+        lb.place(x=295,y=180)
+
+        p = int(entry1.get())
+        count=0
+        for i in range(int(entry2.get()), int(entry3.get())):
+
+                m = get_code(combobox,entry,i)
+
+                pages = shaixuan(m)
+
+
+                print("开始下载第" + str(i) + "页的视频！总共" + str(len(pages)) + '个!')
+                print('\n')
+                headers = {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36'}
+
+                for page in pages:
+                    try:
+                        p1["value"] = 0
+                        wbb = 'https://www.xvideos.com/video' + page + '/'  # 获取包含视频播放链接的网页
+                        response = requests.get(wbb, headers=headers)
+                        response.encoding = 'utf-8'
+                        html = response.text
+                        if combobox1.get()=="高":
+                            url = re.findall('html5player.setVideoUrlHigh\(\'(.*?)\'\);', html, re.S)[0]  # 获取视频播放链接
+                        elif combobox1.get()=="低":
+                            url = re.findall('html5player.setVideoUrlLow\(\'(.*?)\'\);', html, re.S)[0]
+                        str1.set('正在爬取第%d个视频......' % p)
+                        print('开始下载第' + str(p) + '个视频')
+
+                        start = time.time()
+                        size = 0
+                        size1 = 0
+                        response = requests.get(url, stream=True, headers=headers,timeout=5)
+
+                        content_size = int(response.headers['content-length'])
+                        chunk_size = 1024
+                        if response.status_code == 200:
+                            str2.set('[视频大小]:%0.2f MB' % (content_size / 1024 / 1024))
+                            print('[文件大小]:%0.2f MB' % (content_size / 1024 / 1024))
+                            with open(str4.get() + '\\' + str(p) + '.mp4', 'wb') as f:
+                                for data in response.iter_content(chunk_size=chunk_size):
+                                    f.write(data)
+                                    size=len(data)
+                                    size1 = size1 + len(data)
+                                    c=size / content_size * 100
+                                    d=size1 / content_size * 100
+                                    p1["value"] = p1["value"] + c
+                                    top.update()
+                                    str3.set('%.2f%%' % d)
+                                    print('\r' + '[下载进度]:%s%.2f%%' % (
+                                        '■' * int(size1 * 50 / content_size), float(size1 / content_size * 100)), end='')
+                        end = time.time()
+
+                    except:
+                        print("\n")
+                        print("此视频下载出现异常，正在跳过………………")
+                        print("\n")
+                        count += 1
+                        continue
+                    else:
+                        print('\n' + '第' + str(p) + '个视频下载完成! 用时%.2f秒' % (end - start))
+                        p+=1
+
+
+                print('\n')
+                print('第' + str(i) + '页的视频全部下载完成！')
+                print('\n')
+        if count<27:
+            tkinter.messagebox.showinfo('提示!','全部视频已下载完成')
+        else:
+            tkinter.messagebox.showwarning('警告','爬取已中断!!!')
+        print('全部视频已下载完成')
+
+        top.mainloop()
+    else:
+        tkinter.messagebox.showwarning('警告!','文本框内容不能为空!')
+
+
+
+
+
+
+
+
+
